b683c2744857dc7e09e4d9d3e48cf462ae42a711
margarita07 <44953296+margarita07@users.noreply.github.com>
2018-11-11 16:51:00 -0600

    Add files via upload

---
+++ src/main/java/com/guia4Margarita/guia4/MainActivity.java
@@ -0,0 +1,115 @@
+package com.guia4Margarita.guia4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+@SuppressWarnings("ALL")
+public class MainActivity extends AppCompatActivity {
+
+    private int PICK_PHOTO=3;
+    private int PICK_PHOTO2=2;
+
+    ImageView imageView;
+    ImageView segunda;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView   = (ImageView) findViewById(R.id.imgFondo);
+        segunda   = (ImageView) findViewById(R.id.segundaimg);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        //INFO: menu_activity es el archivo que he creado dentro la carpeta menu
+        inflater.inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //cuando seleccione una opcion pasa por un switch
+        //para saber cual es
+        switch (item.getItemId()) {
+            case R.id.agregar:
+                agregarIMG(PICK_PHOTO);//llamo a mi funcion
+                return true;
+            case R.id.agregar2:
+                agregarIMG(PICK_PHOTO2);//llamo a mi funcion
+                return true;
+            case R.id.eliminar://llamo a mi funcion
+                eliminarIMG();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    public void agregarIMG(int codigo) {
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, codigo);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PHOTO && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+                imageView.setImageBitmap(bmp);
+            } catch (IOException e) {
+                Toast.makeText(this,"Error Cargando imagen",Toast.LENGTH_SHORT);
+                e.printStackTrace();
+            }
+        }
+
+        if (requestCode == PICK_PHOTO2 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+                segunda.setImageBitmap(bmp);
+            } catch (IOException e) {
+                Toast.makeText(this,"Error Cargando imagen",Toast.LENGTH_SHORT);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    private void eliminarIMG(){
+
+        imageView.setImageDrawable(null);
+        segunda.setImageDrawable(null);
+    }
+}
